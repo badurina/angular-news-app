3bcb631e0836cb43b52122a231f14037993f8281
user385 <badurina.antun@gmail.com>
2017-08-20 17:25:13 +0200

    moved sources to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SourcesListComponent } from './sources/sources-list.component';
+import { SourcesListComponent } from './components/sources/sources-list.component';
 import { SourcesService } from './services/sources.service';
 
 @NgModule({
